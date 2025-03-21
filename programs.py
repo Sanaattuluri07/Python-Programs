7f65ae14366ff0901957f3cbf8fdc4ba9c67da22
Sanaattuluri07 <sanaattuluri@gmail.com>
2025-03-21 08:39:00 +0530

    Add files via upload

---
+++ programs.py
@@ -0,0 +1,31 @@
+"""#to print 1 to 10 numbers using for loop
+n = 11
+for i in range (1,n):
+    print(i)
+
+#to print numbers divisible by 2 using for loop
+n = int(input("enter a number :"))
+for n in range (1,n):
+    if n % 2 == 0:
+        print(n)
+
+# to print numbers from a list
+n = [1,2,3,4,5]
+for i in (n):
+    if i % 2 == 0:
+        print(i," is even")
+    else:
+        print(i," is odd")
+
+
+# to print numbers from a list
+n = [1,2,3,4,5]
+for i in (n):
+    print(i)"
+    """
+# to print 1 to 5 numbers using while
+i = 0
+while (i<5):
+    i+=1
+    print(i)
+    
